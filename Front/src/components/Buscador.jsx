84c627779c54729c55702de46d36b7c326aa3fff
Andres17X <andresaragon2001@gmail.com>
2023-06-07 19:45:08 +0200

    changees

--- Front/src/components/Buscador.jsx
+++ Front/src/components/Buscador.jsx
@@ -1,22 +1,67 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Buscador.css'
-import CMLogo from '../assets/logo.png'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import './Buscador.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Autosuggest from 'react-autosuggest';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbtack, faUser } from '@fortawesome/free-solid-svg-icons'
 
-function Buscador() {
+const Buscador = () => {
+  const dropdownItems = ['Viviendas', 'Terrenos', 'Oficinas'];
+  const [selectedAction, setSelectedAction] = useState(dropdownItems[0]);
+  const [municipios, setMunicipios] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleActionSelect = (action) => {
+    setSelectedAction(action);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const renderDropdownItems = dropdownItems.map((item) => (
+    <Dropdown.Item key={item} eventKey={item}>
+      {item}
+    </Dropdown.Item>
+  ));
+
   return (
     <>
       <br />
-      <div role='img' className='imagenfondo'>dwadwd</div>
+      <center>
+        <div role="img" className="imagenfondo">
+          <section className="search-box">
+            <h1>Tu casa, tu hogar</h1>
+            <form>
+              <div>
+                <div>
+                  <label className="compra-button">Comprar</label>
+                  <label className="alquilar-button">Alquilar</label>
+                </div>
+                <DropdownButton id="dropdown-basic-button" title={selectedAction} onSelect={handleActionSelect}>
+                  {renderDropdownItems.filter((item) => item.props.eventKey !== selectedAction)}
+                </DropdownButton>
+                <div className="search-input">
+                  <FontAwesomeIcon icon={faSearch} className="search-icon" />
+                  <input
+                    type="text"
+                    className="input-field"
+                    placeholder="Buscar..."
+                    value={searchValue}
+                    onChange={handleSearchInputChange}
+                    style={{ color: 'black' }}
+                  />
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+      </center>
     </>
   );
-}
+};
 
 export default Buscador;
