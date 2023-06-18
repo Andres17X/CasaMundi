ba09cc6ce7c2f66d57440850c55f2ceb82982bc0
jose <joseramongarrido175@gmail.com>
2023-06-18 15:31:35 +0200

    test push

--- Back/casa_mundi_api/Controllers/ViviendaVentaController.cs
+++ Back/casa_mundi_api/Controllers/ViviendaVentaController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application;
+using CasaMundi.Domain;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace casa_mundi_api.Controllers
 {
-    [Route("viviendaVentaController")]
+    [Route("/viviendaVentaController")]
     [ApiController]
     public class ViviendaVentaController : ControllerBase
     {
+        [HttpGet("getViviendasVenta")]
+        public void getViviendasEnVenta()
+        {
+            Console.WriteLine("Hola");
 
+        }
     }
 }
